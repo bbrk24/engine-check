86b0cc4acebd86741f15a64c0adb76e1d03ccdd1
William Baker <bbrk24@gmail.com>
2023-03-26 17:01:49 -0400

    syntax cleanup

--- index.js
+++ index.js
@@ -11,24 +11,23 @@ const argv = yargs(process.argv.slice(2))
         default: './package.json',
         describe: 'The location of package.json',
         normalize: true,
-        type: 'string'
+        type: 'string',
     })
     .option('lockfile', {
         default: './package-lock.json',
         describe: 'The location of package-lock.json',
         normalize: true,
-        type: 'string'
+        type: 'string',
     })
     .option('engine', {
         default: 'node',
         describe: 'Which engine to check',
-        type: 'string'
+        type: 'string',
     })
     .option('quiet', {
         alias: 'q',
         describe: "Don't report which packages fail",
         type: 'boolean',
-        hidden: false
     }).argv;
 
 /** @param {Awaited<typeof argv>} argv */
@@ -44,7 +43,7 @@ const run = argv => {
     /** @type {{ engines?: Partial<Record<string, string>>}} */
     const package = JSON.parse(fs.readFileSync(argv.package).toString());
 
-    if (!package.engines?.[argv.engine]) {
+    if (!package.engines || !package.engines[argv.engine]) {
         process.exitCode = 1;
         console.error(`No engines.${[argv.engine]} is present in ${argv.package}.`);
         return;
@@ -62,8 +61,10 @@ const run = argv => {
      */
     const lockfile = JSON.parse(fs.readFileSync(argv.lockfile).toString());
 
-    if (lockfile.lockfileVersion == null || lockfile.lockfileVersion < 2) {
-        error("Older lockfiles don't include the engines object. Please upgrade to lockfile v2 or v3.");
+    if (lockfile.lockfileVersion === undefined || lockfile.lockfileVersion < 2) {
+        error(
+            "Older lockfiles don't include the engines object. Please upgrade to lockfile v2 or v3."
+        );
         return;
     } else if (lockfile.lockfileVersion > 3 && !argv.quiet) {
         console.warn(`Lockfile v${lockfile.lockfileVersion} is newer than expected.`);
@@ -73,9 +74,11 @@ const run = argv => {
      * @type {[string, string, string][]}
      * @ts-expect-error The filter call ensures there's no undefineds */
     const versions = Object.entries(lockfile.packages)
-        .map(/** @returns {[string, string, string | undefined]} */ el =>
-            [el[0].replace(/node_modules\//g, ''), el[1].version, el[1].engines?.[argv.engine]]
-        )
+        .map(/** @returns {[string, string, string | undefined]} */ el => [
+            el[0].replace(/node_modules\//g, ''),
+            el[1].version,
+            el[1].engines && el[1].engines[argv.engine],
+        ])
         .filter(el => el[0] && el[2]);
 
     for (const [packageName, packageVersion, engineVersions] of versions) {
